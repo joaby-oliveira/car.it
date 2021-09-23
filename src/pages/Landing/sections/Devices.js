04770cba530444ea288d7a279230403bb4e71424
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-23 18:06:28 -0300

    feat: finish devices page

--- src/pages/Landing/sections/Devices.js
+++ src/pages/Landing/sections/Devices.js
@@ -1,10 +1,24 @@
+import Image from 'next/image'
 import { Title } from '../../../components/Title'
 import styles from '../Landing.module.scss'
+import devicesDesktop from '../../../../public/devicesDesktop.png'
+import devicesMobile from '../../../../public/devicesMobile.png'
 
 const Devices = () => {
   return (
-    <section className={`${styles.devices} flex mainCenter`}>
+    <section className={`${styles.devices} flex column crossCenter`}>
       <Title className={styles.title} white={true} center={true}>Encontre as <span>melhores opções</span> de automóveis em qualquer dispositivo</Title>
+      <div className={styles.devicesImages}>
+        <Image
+          className={styles.device}
+          
+          src={devicesMobile} alt="Página de carro do Car.it em um smartphone"
+        />
+        <Image
+          className={styles.device}
+          src={devicesDesktop} alt="Página de carro do Car.it em um notebook"
+        />
+      </div>
     </section>
   )
 }
