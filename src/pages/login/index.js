fa2e8a64e50d10207611b3770695f386948b42a0
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2021-09-27 18:05:40 -0300

    refactor: refactor components code lines

---
+++ src/pages/login/index.js
@@ -0,0 +1,15 @@
+import Button from '../../components/Button';
+
+const index = () => {
+  return (
+    <div>
+      <a href="">Esqueceu a senha?</a>
+      <Button href="/">Entrar</Button>
+      <hr />
+
+      <h3>Não é registrado?<a href="">Registre-se</a></h3>
+    </div>
+  )
+}
+
+export default index
