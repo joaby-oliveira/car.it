47178e494cee9d7f97fa18a75e387f6770825b1e
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2021-09-27 19:03:57 -0300

    feat: Add login page and style

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,16 @@
 import Button from '../../components/Button';
+import Box from '../../components/Box';
+import styles from './styles.module.scss';
 
 const index = () => {
   return (
     <div>
-      <a href="">Esqueceu a senha?</a>
-      <Button href="/">Entrar</Button>
-      <hr />
-
-      <h3>Não é registrado?<a href="">Registre-se</a></h3>
+      <Box>
+        <a className={`${styles.forgotPass}`} href="/forgotpass">Esqueceu a senha?</a>
+        <Button fillParent={true} href="/">Entrar</Button>
+        <hr />
+        <h3>Não é registrado?<a href="">Registre-se</a></h3>
+      </Box>
     </div>
   )
 }
