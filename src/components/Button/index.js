5917614548d94bb86b5aa50869d430fcb254f5a0
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-29 17:23:52 -0300

    fix: add hero's button icon right margin

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -7,7 +7,7 @@ import styles from "./styles.module.scss"
 export const Button = ({ href, children, secondary, fillParent }) => {
   return (
     <Link href={href}>
-      <a className={`${styles.button} flex crossCenter ${secondary ? styles.secondary : ''} ${fillParent ? 'fillParent' : ''}`}>{children}</a>
+      <a className={`${styles.button} flex crossCenter ${secondary ? styles.buttonSecondary : ''} ${fillParent ? 'fillParent' : ''}`}>{children}</a>
     </Link>
   )
 }
