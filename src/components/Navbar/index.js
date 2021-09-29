eb32f151aee57d45d2225fb87c185bd2eaeda2ca
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-09-29 19:17:13 -0300

    fix: wrong position of button text

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,19 @@
+// Import Next.js 
+import Link from 'next/link'
+
 // Import styles
 import styles from './styles.module.scss'
 
 // Import button from 
-import {Button} from '../Button'
+import { Button } from '../Button'
 import Logo from '../Logo'
 
 export const Navbar = () => {
   return (
     <nav className={`${styles.navbar} flex spaceBetween`}>
-      <Logo />
+      <Link href="/">
+        <a><Logo /></a>
+      </Link>
       <div className={`${styles.buttonContainer} flex`}>
         <Button secondary={true} href="/login">Entrar</Button>
         <Button href="/criar-conta">Criar Conta</Button>
