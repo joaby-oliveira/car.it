50c84b9d4492c3b7dedadc7bc39f95647096f5b0
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-10-15 22:15:11 -0300

    feat: users authentication

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,12 @@
 // Import Next.js 
 import Link from 'next/link'
 
+// Import Next.js components
+import router from 'next/router'
+
+// Import React hooks
+import {useState} from 'react'
+
 // Import components
 import Head from 'next/head'
 import { Button } from '../../components/Button';
@@ -19,6 +25,27 @@ const Login = () => {
   const email = useForm('email')
   const password = useForm('password')
 
+  const [loginErrorMessage, setLoginErrorMessage] = useState('')
+
+  async function auth(e) {
+    console.log(email.value)
+    e.preventDefault()
+    const result = await fetch('http://localhost:8080/user/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value
+      })
+    })
+    const data = await result.json()
+    if (data.token) {
+      localStorage.setItem('loginToken', data.token)
+      router.push('/home')
+    } else {
+      setLoginErrorMessage('Email ou senha inválidos')
+    }
+  }
   return (
     <div className={`${styles.loginContainer} flex crossCenter mainCenter fullHeight`}>
       <Head>
@@ -38,28 +65,28 @@ const Login = () => {
 
         <div className={`${styles.arrowBox} flex`}>
           <Link href="/">
-            <a href="/"><FiChevronLeft size={30}/></a>
+            <a href="/"><FiChevronLeft size={30} /></a>
           </Link>
         </div>
-        
-          <Box>
-            <Logo className={`${styles.logoLogin}`} />
 
-            <form className={`${styles.formsLogin}`}>
-              <Input label="Email" name="email" {...email} placeholder="Ex.: email@dominio.com"/>
-              <Input label="Senha" name="password" {...password} placeholder="No mínimo 8 caracteres"/>
-              <a className={`${styles.forgotPass}`} href="/forgotpass">Esqueceu a senha?</a>
-            </form>
-            
-            <Button fillParent={true} spacer={true} href="/" >Entrar</Button>
-            {/* <h3 className={`${styles.loginText}`}>Entrar usando outras contas</h3> */}
-            <div className={`${styles.btnContainer} flex`}>
-              <Button fillParent={true} google={true} href="/"> <FaGoogle /> </Button>
-              <Button fillParent={true} facebook={true} href="/"> <FaFacebookF /> </Button>
-            </div>
-            <h3 className={`${styles.loginText}`}>Não é cadastrado?<a href="/"> Registre-se</a></h3>
-          </Box>
-        
+        <Box>
+          <Logo className={`${styles.logoLogin}`} />
+
+          <form onSubmit={auth} className={`${styles.formsLogin}`}>
+            <Input label="Email" name="email" {...email} placeholder="Ex.: email@dominio.com" />
+            <Input label="Senha" name="password" {...password} placeholder="No mínimo 8 caracteres" />
+            <a className={`${styles.forgotPass}`} href="/forgotpass">Esqueceu a senha?</a>
+            <Button form={true} fillParent={true} spacer={true} href="/" >Entrar</Button>
+          </form>
+
+          <h3 className={`${styles.loginText}`}>{loginErrorMessage}</h3>
+          <div className={`${styles.btnContainer} flex`}>
+            <Button fillParent={true} google={true} href="/"> <FaGoogle /> </Button>
+            <Button fillParent={true} facebook={true} href="/"> <FaFacebookF /> </Button>
+          </div>
+          <h3 className={`${styles.loginText}`}>Não é cadastrado?<a href="/"> Registre-se</a></h3>
+        </Box>
+
       </div>
     </div>
   )
