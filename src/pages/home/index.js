2a8ac1aa68bcb76cc20f164468a5bf5335f52519
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-10-15 22:13:21 -0300

    feat: create home page

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import { Navbar } from '../../components/Navbar'
+import styles from './styles.module.scss'
+const Home = () => {
+  return (
+    <div animationType="leftToRight">
+      <Navbar />
+      Pagina inicial
+    </div>
+  )
+}
+
+export default Home
