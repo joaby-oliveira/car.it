5582f83142c0c61753fd4013256ea6c6794a3dc3
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2021-10-15 22:30:53 -0300

    feat: add new button properties

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,16 +4,17 @@ import Link from "next/link"
 // Import styles
 import styles from "./styles.module.scss"
 
-export const Button = ({ href, children, secondary, fillParent, facebook, google, spacer }) => {
+export const Button = ({ href, children, secondary, fillParent, facebook, google, spacer, onClick }) => {
   return (
     <Link href={href}>
-      <a className={` 
-        ${styles.button} flex crossCenter mainCenter 
-        ${secondary ? styles.buttonSecondary : ''} 
-        ${fillParent ? styles.fillParent : ''} 
-        ${facebook ? styles.facebook : ''}
-        ${google ? styles.google : ''}
-        ${spacer ? styles.spacer : ''}
+      <a
+        className={` 
+          ${styles.button} flex crossCenter mainCenter 
+          ${secondary ? styles.buttonSecondary : ''} 
+          ${fillParent ? styles.fillParent : ''} 
+          ${facebook ? styles.facebook : ''}
+          ${google ? styles.google : ''}
+          ${spacer ? styles.spacer : ''}
       `}>{children}</a>
     </Link>
   )
