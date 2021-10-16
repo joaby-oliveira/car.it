f43c028e3f5cadb536ab5f8a006e55968d44ab58
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-10-15 22:10:02 -0300

    feat: create navbar

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,32 @@
 // Import  Next.js components
 import Link from "next/link"
 
+import { FiSearch } from 'react-icons/fi'
+
 // Import styles
 import styles from "./styles.module.scss"
 
-export const Button = ({ href, children, secondary, fillParent, facebook, google, spacer }) => {
+export const Button = ({ href, children, form, secondary, fillParent, facebook, google, spacer, searchCar }) => {
+  if (searchCar) {
+    return (
+      <div className={` 
+      ${styles.button} flex crossCenter mainCenter 
+      ${styles.searchButton}
+    `}><FiSearch />Achar carros</div>
+    )
+  }
+
+  if (form) {
+    return (
+      <button className={` 
+      ${styles.button} flex crossCenter mainCenter 
+      ${styles.searchButton}
+    `}>
+        {children}
+      </button>
+    )
+  }
+
   return (
     <Link href={href}>
       <a className={` 
