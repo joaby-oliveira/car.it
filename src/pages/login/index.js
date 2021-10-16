20dcff607c6038228478022f4f366a77babe1508
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2021-10-15 22:36:36 -0300

    feat: add page components and style

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,11 @@
-// Import Next.js 
-import Link from 'next/link'
-
 // Import components
 import Head from 'next/head'
 import { Button } from '../../components/Button';
 import { Box } from '../../components/Box';
 import { Input } from '../../components/Input';
 import { FaFacebookF, FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa";
-import { FiChevronLeft } from "react-icons/fi";
-import LoginImage from '../../components/LoginImage';
-import Logo from '../../components/Logo';
+import { FormsLS } from '../../components/FormsLS';
+import { ArrowBox } from "../../components/ArrowBox";
 
 // Import styles 
 import styles from './styles.module.scss';
@@ -27,39 +23,30 @@ const Login = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div animationType="leftToRight" className={`${styles.loginLS} flex column mainCenter crossCenter`}>
-        <Logo className={`${styles.logoLogin}`} white={true} />
-        <h1 className={`${styles.loginTitle}`}>Faça seu login</h1>
-        <p className={`${styles.loginSubtitle}`}>Bem vindo de volta!</p>
-        <LoginImage />
-      </div>
 
-      <div animationType="rightToLeft" className={`${styles.loginRS} flex mainCenter`}>
+      <FormsLS title="Faça seu login" subtitle="Bem vindo de volta!" />
 
-        <div className={`${styles.arrowBox} flex`}>
-          <Link href="/">
-            <a href="/"><FiChevronLeft size={30}/></a>
-          </Link>
-        </div>
-        
-          <Box>
-            <Logo className={`${styles.logoLogin}`} />
 
-            <form className={`${styles.formsLogin}`}>
-              <Input label="Email" name="email" {...email} placeholder="Ex.: email@dominio.com"/>
-              <Input label="Senha" name="password" {...password} placeholder="No mínimo 8 caracteres"/>
-              <a className={`${styles.forgotPass}`} href="/forgotpass">Esqueceu a senha?</a>
-            </form>
-            
-            <Button fillParent={true} spacer={true} href="/" >Entrar</Button>
-            {/* <h3 className={`${styles.loginText}`}>Entrar usando outras contas</h3> */}
-            <div className={`${styles.btnContainer} flex`}>
-              <Button fillParent={true} google={true} href="/"> <FaGoogle /> </Button>
-              <Button fillParent={true} facebook={true} href="/"> <FaFacebookF /> </Button>
-            </div>
-            <h3 className={`${styles.loginText}`}>Não é cadastrado?<a href="/"> Registre-se</a></h3>
-          </Box>
-        
+      <div animationType="rightToLeft" className={`formsRS flex mainCenter`}>
+
+      <ArrowBox href="/" />
+
+        <Box>
+
+          <form className={`forms`}>
+            <Input label="Email" name="email" {...email} placeholder="Ex.: email@dominio.com" />
+            <Input label="Senha" name="password" {...password} placeholder="No mínimo 8 caracteres" />
+            <a className={`${styles.forgotPass}`} href="/forgotpass">Esqueceu a senha?</a>
+          </form>
+
+          <Button fillParent={true} spacer={true} href="/" >Entrar</Button>
+          <div className={`${styles.btnContainer} flex`}>
+            <Button fillParent={true} google={true} href="/"> <FaGoogle /> </Button>
+            <Button fillParent={true} facebook={true} href="/"> <FaFacebookF /> </Button>
+          </div>
+          <h3 className={`${styles.loginText}`}>Não é cadastrado?<a href="/register"> Registre-se</a></h3>
+        </Box>
+
       </div>
     </div>
   )
