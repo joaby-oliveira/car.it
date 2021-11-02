45fbc956dec78c757b63a5da453cae81356fff40
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-02 08:42:48 -0300

    feat: add link to profile page

--- src/components/ProfilePreview/index.js
+++ src/components/ProfilePreview/index.js
@@ -1,15 +1,22 @@
 import Image from 'next/image'
+import Link from 'next/Link'
 
 import styles from './styles.module.scss'
+
 import rodriguesLogo from '../../../public/rodriguesLogo.jpg'
 import stars from '../../../public/stars.svg'
+
 
 export const ProfilePreview = () => {
   return (
     <div className={`flex`}>
       <Image className={`${styles.userLogo}`} src={rodriguesLogo} />
       <div className={`${styles.details}`}>
-        <h1 className={`${styles.userName}`}>Rodrigues Veículos</h1>
+        <Link href='/profile'>
+          <a>
+            <h1 className={`${styles.userName}`}>Rodrigues Veículos</h1>
+          </a>
+        </Link>
         <small className={`${styles.userRate} flex`}>
           <p>4.9/5.0</p>
           <Image src={stars} />
