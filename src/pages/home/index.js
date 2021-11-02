603906d1556c17b257ab6b03ec42243b71b0eec3
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-02 08:43:10 -0300

    feat: create profile bar at home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,33 @@
 import { Navbar } from '../../components/Navbar'
 import { ProfilePreview } from '../../components/ProfilePreview'
 
+import Link from 'next/link'
+
+import { FiSettings, FiBell } from 'react-icons/fi'
+
 import styles from './styles.module.scss'
 
 const Home = () => {
   return (
     <div className={`page`} animationType="leftToRight">
       <Navbar />
-      <ProfilePreview />
-      Pagina inicial
+      <div className={`${styles.container}`}>
+        <div className={`${styles.profileBar} flex spaceBetween crossCenter`}>
+          <ProfilePreview />
+          <div className={`${styles.profileActions} flex`}>
+            <Link href="/settings">
+              <a className={`${styles.button} flex mainCenter crossCenter`}>
+                <FiSettings size={24} />
+              </a>
+            </Link>
+            <Link href="/notification">
+              <a className={`${styles.button} flex mainCenter crossCenter`}>
+                <FiBell size={24} />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
