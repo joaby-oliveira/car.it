d228f3319ef7897cd7c759fd61ab21ee04606da3
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-08 09:09:37 -0300

    feat: create car preview component

---
+++ src/components/CarPreview/index.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { FiHeart } from 'react-icons/fi'
+
+import car from '../../../public/car.jpg'
+import { Text } from '../Text'
+import { Title } from '../Title'
+
+import styles from './styles.module.scss'
+
+export const CarPreview = () => {
+  return (
+    <Link href="/car">
+      <a className={`${styles.carPreview} flex column crossCenter`}>
+        <div className={`${styles.carImageContainer}`}>
+          <Image className={`${styles.carImage}`} src={car} />
+        </div>
+        <div className={`${styles.carDetails} flex column`}>
+          <Title small={true} className={`${styles.carName}`}>Chevrolet Onix</Title>
+          <div className={`${styles.description} flex column`}>
+            <Text>1.0 Flex LT Manual</Text>
+            <Text>2014/2014</Text>
+            <small className={`${styles.seller}`}>Jorge de Souza</small>
+            <FiHeart size={30} className={`${styles.favorite}`} />
+          </div>
+          <h2 className={`${styles.price}`}>R$ 35.500</h2>
+        </div>
+      </a>
+    </Link>
+  )
+}
