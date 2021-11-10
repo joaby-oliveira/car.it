711676b9603aad33aec156d387078ab54fbe71a8
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-10 11:38:59 -0300

    feat: add description to action button

--- src/components/ActionButton/index.js
+++ src/components/ActionButton/index.js
@@ -6,26 +6,34 @@ import styles from './styles.module.scss'
 import fipeIlustration from '../../../public/fipeIlustration.svg'
 import garageIlustration from '../../../public/garageIlustration.svg'
 
-export const ActionButton = ({fipe, garage}) => {
+export const ActionButton = ({ fipe, garage }) => {
   return (
-    <div className={`${styles.actionButton} ${fipe ? styles.fipe : ''} ${garage ? styles.garage : ''} flex column crossCenter spaceBetween`}>
-      <div className={`${styles.imageContainer} flex crossCenter`}>
-        <Image src={fipe ? fipeIlustration : '' || garage ? garageIlustration : ''} />
+    <div className={`${styles.actionButtonContainer}`}>
+      <div className={`${styles.actionButton} ${fipe ? styles.fipe : ''} ${garage ? styles.garage : ''} flex column crossCenter spaceBetween`}>
+        <div className={`${styles.imageContainer} flex crossCenter`}>
+          <Image src={fipe ? fipeIlustration : '' || garage ? garageIlustration : ''} />
+        </div>
+        <Link href='/fipe'>
+          <a className={`${styles.cta} flex mainCenter crossCenter`}>
+            {fipe && (
+              <>
+                FIPE
+              </>
+            )}
+            {garage && (
+              <>
+                Garagem
+              </>
+            )}
+          </a>
+        </Link>
       </div>
-      <Link href='/fipe'>
-        <a className={`${styles.cta} flex mainCenter crossCenter`}>
-          {fipe && (
-            <>
-              FIPE
-            </>
-          )}
-          {garage && (
-            <>
-              Garagem
-            </>
-          )}
-        </a>
-      </Link>
+      {fipe && (
+        <p className={`${styles.description}`}>Procure a FIPE do carro que deseja</p>
+      )}
+      {garage && (
+        <p className={`${styles.description}`}>Carros que você anunciou</p>
+      )}
     </div>
   )
 }
