e97ca04c9dd1bd78c2b9f72683e7c60be19a628b
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-09 22:07:43 -0300

    feat: add slider to action list container scroll effect

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,12 +5,56 @@ import Link from 'next/link'
 
 import { FiSettings, FiBell } from 'react-icons/fi'
 
+// Import libs
+import Slider from "react-slick"
+
+// Import slick css styles 
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+
 import styles from './styles.module.scss'
 import { CarPreview } from '../../components/CarPreview'
 import { Title } from '../../components/Title'
 import { ActionButton } from '../../components/ActionButton'
 
 const Home = () => {
+  const configs = {
+    className: "slider variable-width",
+    dots: true,
+    infinite: false,
+    slidesToShow: 1,
+    variableWidth: true,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1365,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+  }
+
   return (
     <div className={`page`} animationType="leftToRight">
       <Navbar />
@@ -34,23 +78,25 @@ const Home = () => {
           <Title small={true}>
             Sugestões para você
           </Title>
-          <div className={`${styles.actionsList} flex`}>
-            <ActionButton fipe={true}/>
-            <ActionButton fipe={true}/>
-            <ActionButton fipe={true}/>
-            <ActionButton fipe={true}/>
-            <ActionButton fipe={true}/>
-          </div>
+
+          <Slider {...configs}>
+            <ActionButton fipe={true} />
+            <ActionButton fipe={true} />
+            <ActionButton fipe={true} />
+            <ActionButton fipe={true} />
+          </Slider>
+
         </div>
         <div className={`${styles.actionsListContainer} flex column`}>
           <Title small={true}>
             Talvez goste desses carros
           </Title>
-          <div className={`${styles.actionsList}`}>
-            <div className="actionsList">
-              <CarPreview />
-            </div>
-          </div>
+          <Slider {...configs}>
+            <CarPreview fipe={true} />
+            <CarPreview fipe={true} />
+            <CarPreview fipe={true} />
+            <CarPreview fipe={true} />
+          </Slider>
         </div>
       </div>
     </div>
