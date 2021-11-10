3cd0977f2e5344cb0cc947333fe12df40e0e7e81
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-09 22:07:06 -0300

    feat: see more on hover button

--- src/components/CarPreview/index.js
+++ src/components/CarPreview/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { FiHeart } from 'react-icons/fi'
 
 import car from '../../../public/car.jpg'
+import { Button } from '../Button'
 import { Text } from '../Text'
 import { Title } from '../Title'
 
@@ -11,22 +12,23 @@ import styles from './styles.module.scss'
 
 export const CarPreview = () => {
   return (
-    <Link href="/car">
-      <a className={`${styles.carPreview} flex column crossCenter`}>
-        <div className={`${styles.carImageContainer}`}>
-          <Image className={`${styles.carImage}`} src={car} />
+    <div className={`${styles.carPreview} flex column crossCenter`}>
+      <div className={`${styles.carImageContainer}`}>
+        <Image className={`${styles.carImage}`} src={car} />
+      </div>
+      <div className={`${styles.carDetails} flex column`}>
+        <Title small={true} className={`${styles.carName}`}>Chevrolet Onix</Title>
+        <div className={`${styles.description} flex column`}>
+          <Text>1.0 Flex LT Manual</Text>
+          <Text>2014/2014</Text>
+          <small className={`${styles.seller}`}>Jorge de Souza</small>
+          <FiHeart size={30} className={`${styles.favorite}`} />
         </div>
-        <div className={`${styles.carDetails} flex column`}>
-          <Title small={true} className={`${styles.carName}`}>Chevrolet Onix</Title>
-          <div className={`${styles.description} flex column`}>
-            <Text>1.0 Flex LT Manual</Text>
-            <Text>2014/2014</Text>
-            <small className={`${styles.seller}`}>Jorge de Souza</small>
-            <FiHeart size={30} className={`${styles.favorite}`} />
-          </div>
-          <h2 className={`${styles.price}`}>R$ 35.500</h2>
+        <h2 className={`${styles.price}`}>R$ 35.500</h2>
+        <div className={styles.more}>
+          <Button href="/car" fillParent={true}>Ver carro</Button>
         </div>
-      </a>
-    </Link>
+      </div>
+    </div>
   )
 }
