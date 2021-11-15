a32a49b305c7361f912a78555ddf283bb0966f5e
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-15 13:09:09 -0300

    feat: create profile preview component

--- src/components/ProfilePreview/index.js
+++ src/components/ProfilePreview/index.js
@@ -8,9 +8,9 @@ import stars from '../../../public/stars.svg'
 
 import { Title } from '../Title'
 
-export const ProfilePreview = ({isProfilePage}) => {
+export const ProfilePreview = ({isProfilePage, className}) => {
   return (
-    <div className={`${styles.previewContainer} ${isProfilePage ? styles.profile : ''} flex`}>
+    <div className={`${className} ${styles.previewContainer} ${isProfilePage ? styles.profile : ''} flex`}>
       <Image className={`${styles.userLogo}`} src={rodriguesLogo} />
       <div className={`${styles.details}`}>
         <Link href='/profile'>
