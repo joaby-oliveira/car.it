920a42e73ee556b591c46ce7aa9c6b4e74952f6d
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-15 13:06:25 -0300

    feat: create box different types

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,9 +1,9 @@
 // Import styles
 import styles from "./styles.module.scss"
 
-export const Box = ({ children }) => {
+export const Box = ({ children, padding = "medium" }) => {
   return (
-    <div className={`${styles.box} flex column mainCenter crossCenter`}>
+    <div className={`${styles.box} ${padding === 'small' ? styles.small : ''} ${padding === 'medium' ? styles.medium : ''} ${padding === 'big' ? styles.big : ''} flex column mainCenter crossCenter`}>
       {children}
     </div>
   )
