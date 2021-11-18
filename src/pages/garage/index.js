dda57b8a8349b5d9879f193fd19b9ca5a9a7f1a6
Gabriel Ragonha <gabrielRagonhaProfissional@outlook.com>
2021-11-18 11:08:31 -0200

    feat: add garage page and style

--- src/pages/garage/index.js
+++ src/pages/garage/index.js
@@ -1,9 +1,26 @@
+import styles from './styles.module.scss'
+
 import { Navbar } from "../../components/Navbar"
+import { NoneItems } from "../../components/NoneItems"
+import Head from 'next/head'
 
 const Garage = () => {
+  const cars = true;
   return (
-    <main>
+    <main className="flex">
+      <Head>
+        <title>Car.it | Meus Carros</title>
+        <meta name="description" content="Anuncie e compre carros novos e usados na cidade de Marília" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
+      <div className={`${styles.container} flex column`}>
+        {cars && (
+          <div>
+            <NoneItems page='garage' />
+          </div>
+        )}
+      </div>
     </main>
 
   )
