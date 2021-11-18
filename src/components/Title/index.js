0107e7e050082e8bd34135d75626e4bd407c0292
Gabriel Ragonha <gabrielRagonhaProfissional@outlook.com>
2021-11-18 11:14:21 -0200

    feat: add medium property and style

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,10 @@
 // Import styles
 import styles from "./styles.module.scss"
 
-export const Title = ({ white, center, small, children }) => {
-  if (small) {
+export const Title = ({ white, center, small, medium, children }) => {
+  if (small || medium) {
     return (
-      <h2 className={`${styles.title} ${white ? styles.white : ''} ${center ? styles.center : ''} ${small ? styles.small : ''}`}>
+      <h2 className={`${styles.title} ${white ? styles.white : ''} ${center ? styles.center : ''} ${small ? styles.small : ''} ${medium ? styles.medium : ''}`}>
         {children}
       </h2>
     )
