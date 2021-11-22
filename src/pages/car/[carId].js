d1a5d2a07e2a437dc02395dbffcdb7679c779f5f
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-11-22 16:47:21 -0300

    feat: create unique car page

---
+++ src/pages/car/[carId].js
@@ -0,0 +1,94 @@
+import { FiPhone, FiMessageSquare } from 'react-icons/fi'
+
+import { useRouter } from "next/dist/client/router"
+import Image from 'next/image'
+
+import styles from './styles.module.scss'
+
+// Import libs
+import Slider from "react-slick"
+
+// Import slick css styles 
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+// Import images
+import suv from "../../../public/suv.png"
+import hatch from "../../../public/hatch.png"
+import minivan from "../../../public/minivan.png"
+import sedan from "../../../public/sedan.png"
+import coupe from "../../../public/coupe.png"
+
+import { Title } from '../../components/Title'
+import { Text } from '../../components/Text'
+import { Navbar } from '../../components/Navbar'
+
+const carId = () => {
+
+  const carImages = [
+    suv,
+    hatch,
+    minivan,
+    sedan,
+    coupe
+  ]
+
+  const configs = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    appendDots: dots => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+          marginTop: "-20px"
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    )
+  }
+
+  const router = useRouter()
+  const { carId } = router.query
+
+  return (
+    <main className={`flex`}>
+      <Navbar />
+      <div className={`${styles.container}`}>
+        <Slider className={`${styles.carousel} flex`} {...configs}>
+          {
+            carImages.map((carImage) => {
+              return (
+                <Image src={carImage} />
+              )
+            })
+          }
+        </Slider>
+        <div className={`${styles.rightSide} flex column`}>
+          <Title>Chevrolet Onix</Title>
+          <h2 className={`${styles.price}`}>R$42.400</h2>
+          <Text>2014 | 67.542 km</Text>
+          <div className={`${styles.actionButtonContainer} flex`}>
+            <span className={`${styles.actionButton} ${styles.phone} flex mainCenter crossCenter `}><FiPhone size={24} /></span>
+            <span className={`${styles.actionButton} ${styles.chat} flex mainCenter crossCenter `}><FiMessageSquare size={24} /></span>
+          </div>
+        </div>
+
+        <div className={styles.description}>
+          <Title small={true}>
+            Descrição
+          </Title>
+          <Text>Sem detalhes, 700 de doc, documento em branco. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at metus id purus pellentesque viverra vitae quis nisi. Morbi egestas hendrerit erat eu consectetur. Phasellus auctor sit amet magna in elementum. Vivamus consequat venenatis ligula, vitae scelerisque odio sodales id.</Text>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default carId
